8b3224af2a70b9ca6d95bbaeb4de924af664a096
CristinaCasch <criscastro1926@hotmail.com>
2021-04-13 00:18:05 +0000

    almost-finished

--- src/js/views/planetDetails.js
+++ src/js/views/planetDetails.js
@@ -19,63 +19,65 @@ const PlanetDetails = () => {
 
 	return (
 		<Container>
-			<Jumbotron className="mt-5">
-				<Media>
-					<Row>
-						<Col>
-							<Image
-								width={500}
-								height={300}
-								className="mr-3"
-								src="https://i.pinimg.com/originals/b9/bc/ba/b9bcba104d09193da79ffc90c1fa2dc7.jpg"
-								alt="Generic placeholder"
-							/>
-						</Col>
-						<Col>
-							<Media.Body>
-								<h5>Heading</h5>
-								<p>
-									Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante
-									sollicitudin commodo. Cras purus odio, vestibulum in vulputate at, tempus viverra
-									turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue
-									felis in faucibus.
-								</p>
-							</Media.Body>
-						</Col>
-					</Row>
-				</Media>
-				<Table responsive="sm">
-					<thead>
-						<tr>
-							<th>Name</th>
-							<th>Rotation Period</th>
-							<th>Orbital Period</th>
-							<th>Diameter</th>
-							<th>Climate</th>
-							<th>gravity</th>
-							<th>Terrain</th>
-							<th>Surface Water</th>
-							<th>Population</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr>
-							<td>{item.name}</td>
-							<td>{item.rotation_period}</td>
-							<td>{item.orbital_period}</td>
-							<td>{item.diameter}</td>
-							<td>{item.climate}</td>
-							<td>{item.gravity}</td>
-							<td>{item.terrain}</td>
-							<td>{item.surface_water}</td>
-							<td>{item.population}</td>
-						</tr>
-					</tbody>
-				</Table>
-			</Jumbotron>
-			<Link to="/">
-				<Button variant="primary">Back to home</Button>
-			</Link>
+			{store.planetList.map((item, index) => {
+				return (
+					<Jumbotron className="mt-5" key={index}>
+						<Media>
+							<Row>
+								<Col>
+									<Image
+										width={500}
+										height={300}
+										className="mr-3"
+										src="https://i.pinimg.com/originals/b9/bc/ba/b9bcba104d09193da79ffc90c1fa2dc7.jpg"
+										alt="Generic placeholder"
+									/>
+								</Col>
+								<Col>
+									<Media.Body>
+										<h5>{item.name}</h5>
+										<p>
+											Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque
+											ante sollicitudin commodo. Cras purus odio, vestibulum in vulputate at,
+											tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla.
+											Donec lacinia congue felis in faucibus.
+										</p>
+									</Media.Body>
+								</Col>
+							</Row>
+						</Media>
+						<Table responsive="sm">
+							<thead>
+								<tr>
+									<th>Rotation Period</th>
+									<th>Orbital Period</th>
+									<th>Diameter</th>
+									<th>Climate</th>
+									<th>gravity</th>
+									<th>Terrain</th>
+									<th>Surface Water</th>
+									<th>Population</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td>{item.rotation_period}</td>
+									<td>{item.orbital_period}</td>
+									<td>{item.diameter}</td>
+									<td>{item.climate}</td>
+									<td>{item.gravity}</td>
+									<td>{item.terrain}</td>
+									<td>{item.surface_water}</td>
+									<td>{item.population}</td>
+								</tr>
+							</tbody>
+						</Table>
+						<Link to="/">
+							<Button variant="primary">Back to home</Button>
+						</Link>
+					</Jumbotron>
+				);
+			})}
 		</Container>
 	);
 };
